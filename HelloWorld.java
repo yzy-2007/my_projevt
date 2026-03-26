2d7bf86a4aae1eeb4cd4b6643037ea26a74842f4
yzy <1812759869@qq.com>
2026-03-26 12:48:23 +0800

    change HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,2 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("HelloWorld"); } }
+public class HelloWorld { public static void main(String[] args) { System.out.println("HelloWorld");
+System.out.println("HelloWorld"); } }
